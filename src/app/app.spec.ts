cbefca6d981bc9b9c90f2eab201e8f69928eed74
Eric Albert <eric.albert8787@gmail.com>
2026-09-16 10:34:25 -0400

    Changes

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,24 +1,17 @@
-import { TestBed } from '@angular/core/testing';
-import { App } from './app';
-
-describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    })
-      .compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, lab1-temp-converter');
-  });
-});
+	import { TestBed } from '@angular/core/testing';
+	import { App } from './app';
+	import {TempConverter} from './temp-converter/temp-converter';
+	
+	describe('App', () => {
+	  beforeEach(async () => {
+	    await TestBed.configureTestingModule({
+	      imports: [App, TempConverter],
+	    }).compileComponents();
+	  });
+	
+	  it('should create the app', () => {
+	    const fixture = TestBed.createComponent(App);
+	    const app = fixture.componentInstance;
+	    expect(app).toBeTruthy();
+	  });
+	});
---
+++ src/app/temp-converter./temp-converter..spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TempConverter } from './temp-converter.';
+
+describe('TempConverter', () => {
+  let component: TempConverter;
+  let fixture: ComponentFixture<TempConverter>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TempConverter],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TempConverter);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should convert 0 celsius to 32 fahrenheit', () => {
+	    const tempval = '0';
+	    component.convertCelsius(tempval);
+	    expect(component.fahrenheitValue).toBeCloseTo(32);
+	  });
+	
+	  it('should convert -100 fahrenheit to -73.33 celsius', () => {
+	    const tempval = '-100';
+	    component.convertFahrenheit(tempval);
+	    expect(component.celsiusValue).toBeCloseTo(-73.33);
+	});
+});
